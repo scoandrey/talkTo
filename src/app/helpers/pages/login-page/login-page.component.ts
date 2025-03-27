b7e2b646bb56122f413166cb08a6a9cbc8f21f5e
Andrii <scoandrey@gmail.com>
2025-03-27 16:56:44 -0400

    form

--- src/app/helpers/pages/login-page/login-page.component.ts
+++ src/app/helpers/pages/login-page/login-page.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
   imports: [],
   templateUrl: './login-page.component.html',
-  styleUrl: './login-page.component.scss'
+  styleUrl: './login-page.component.scss',
 })
 export class LoginPageComponent {
-
+  form = new FormGroup({
+    username: new FormControl(null),
+    password: new FormControl(null),
+  });
 }
