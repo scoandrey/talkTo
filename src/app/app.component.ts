f744fb1d46386843713c935b485cd62509c2b9b2
Andrii <scoandrey@gmail.com>
2025-03-26 23:26:11 -0400

    login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProfileCardComponent } from './common-ui/profile-card/profile-card.component';
 import { CommonModule } from '@angular/common';
+import { SidebarComponent } from './common-ui/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ProfileCardComponent],
+  imports: [CommonModule, RouterOutlet, ProfileCardComponent, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,21 @@
 import { Routes } from '@angular/router';
 import { LoginPageComponent } from './helpers/pages/login-page/login-page.component';
 import { SearchPageComponent } from './helpers/pages/search-page/search-page.component';
+import { ProfilePageComponent } from './helpers/pages/profile-page/profile-page.component';
+import { LayoutComponent } from './common-ui/layout/layout.component';
 
 export const routes: Routes = [
-  { path: '', component: SearchPageComponent },
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: '', component: SearchPageComponent },
+      {
+        path: 'profile',
+        component: ProfilePageComponent,
+      },
+    ],
+  },
   {
     path: 'login',
     component: LoginPageComponent,
