b90c657736408996c34b86e34cc2598ba04342c1
Andrii <scoandrey@gmail.com>
2025-03-27 23:13:56 -0400

    form

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,13 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  http = inject(HttpClient);
+  baseApiUrl = 'https://icherniakov.ru/yt-course/auth';
+  login(payload: { username: string; password: string }) {
+    return this.http.post(`${this.baseApiUrl}token`, payload);
+  }
+}
