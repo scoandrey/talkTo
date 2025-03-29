001cf82bcfc9fc2550c37d6fcb336629064f7b2a
Andrii <scoandrey@gmail.com>
2025-03-28 23:12:51 -0400

    form

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -6,7 +6,7 @@ import { inject, Injectable } from '@angular/core';
 })
 export class AuthService {
   http = inject(HttpClient);
-  baseApiUrl = 'https://icherniakov.ru/yt-course/auth';
+  baseApiUrl = 'https://icherniakov.ru/yt-course/auth/';
   login(payload: { username: string; password: string }) {
     return this.http.post(`${this.baseApiUrl}token`, payload);
   }
--- src/app/helpers/pages/login-page/login-page.component.ts
+++ src/app/helpers/pages/login-page/login-page.component.ts
@@ -1,16 +1,32 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../../../auth/auth.service';
 
+interface LoginForm {
+  username: string;
+  password: string;
+}
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login-page.component.html',
-  styleUrl: './login-page.component.scss',
+  styleUrls: ['./login-page.component.scss'],
 })
 export class LoginPageComponent {
-  form = new FormGroup({
-    username: new FormControl(null),
-    password: new FormControl(null),
+  private authService = inject(AuthService);
+  private fb = inject(FormBuilder);
+
+  form = this.fb.nonNullable.group({
+    username: ['', Validators.required],
+    password: ['', Validators.required],
   });
+
+  onSubmit(): void {
+    if (this.form.valid) {
+      const payload: LoginForm = this.form.getRawValue();
+      this.authService.login(payload);
+    }
+  }
 }
