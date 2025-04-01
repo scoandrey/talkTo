0ae4085ffa2f10a377c44c567e84b823de179c5e
Andrii <scoandrey@gmail.com>
2025-03-31 23:47:39 -0400

    login

--- src/app/auth/auth.interceptor.ts
+++ src/app/auth/auth.interceptor.ts
@@ -5,20 +5,20 @@ import {
   HttpRequest,
 } from '@angular/common/http';
 import { inject } from '@angular/core';
-import { catchError, throwError } from 'rxjs';
+import { catchError, switchMap, throwError } from 'rxjs';
 import { AuthService } from './auth.service';
+
+let isRefresh = false;
 
 export const authTokenInterceptor: HttpInterceptorFn = (req, next) => {
   const authService = inject(AuthService);
   const token = authService.token;
   if (!token) return next(req);
+  if (isRefresh) {
+    return refreshAndProceed(authService, req, next);
+  }
 
-  req = req.clone({
-    setHeaders: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
-  return next(req).pipe(
+  return next(addToken(req, token)).pipe(
     catchError((error) => {
       if (error.status === 403) {
         return refreshAndProceed(authService, req, next);
@@ -33,5 +33,22 @@ const refreshAndProceed = (
   req: HttpRequest<any>,
   next: HttpHandlerFn
 ) => {
-  return;
+  if (!isRefresh) {
+    isRefresh = true;
+    return authService.refreshAuthToken().pipe(
+      switchMap((res) => {
+        isRefresh = false;
+        return next(addToken(req, res.access_token));
+      })
+    );
+  }
+  return next(addToken(req, authService.token!));
+};
+
+const addToken = (req: HttpRequest<any>, token: string) => {
+  return req.clone({
+    setHeaders: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
 };
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,15 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { take, tap } from 'rxjs';
+import { catchError, tap, throwError } from 'rxjs';
 import { TokenResponse } from './auth.interface';
 import { CookieService } from 'ngx-cookie-service';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   http = inject(HttpClient);
-  coockieService = inject(CookieService);
+  router = inject(Router);
+  cookieService = inject(CookieService);
   baseApiUrl = 'https://icherniakov.ru/yt-course/auth/';
 
   token!: string | null;
@@ -17,21 +19,49 @@ export class AuthService {
 
   get isAuth() {
     if (!this.token) {
-      this.token = this.coockieService.get('token');
+      this.token = this.cookieService.get('token') || null;
+      this.refreshToken = this.cookieService.get('refreshToken') || null;
     }
     return !!this.token;
   }
+
   login(payload: { username: string; password: string }) {
-    const fd: FormData = new FormData();
+    const fd = new FormData();
     fd.append('username', payload.username);
     fd.append('password', payload.password);
-    return this.http.post<TokenResponse>(`${this.baseApiUrl}token`, fd).pipe(
-      tap((val) => {
-        this.token = val.access_token;
-        this.refreshToken = val.refresh_token;
-        this.coockieService.set('token', this.token);
-        this.coockieService.set('refreshToken', this.refreshToken);
+    return this.http
+      .post<TokenResponse>(`${this.baseApiUrl}token`, fd)
+      .pipe(tap((val) => this.saveTokens(val)));
+  }
+
+  refreshAuthToken() {
+    return this.http
+      .post<TokenResponse>(`${this.baseApiUrl}refresh`, {
+        refresh_token: this.refreshToken,
       })
-    );
+      .pipe(
+        tap((val) => this.saveTokens(val)),
+        catchError((error) => {
+          this.logout();
+          return throwError(() => error);
+        })
+      );
+  }
+
+  logout() {
+    this.cookieService.deleteAll();
+    this.token = null;
+    this.refreshToken = null;
+    this.router.navigate(['/login']);
+  }
+
+  private saveTokens(res: TokenResponse) {
+    this.token = res.access_token;
+    this.refreshToken = res.refresh_token;
+    this.cookieService.set('token', this.token, { expires: 7, path: '/' });
+    this.cookieService.set('refreshToken', this.refreshToken, {
+      expires: 7,
+      path: '/',
+    });
   }
 }
